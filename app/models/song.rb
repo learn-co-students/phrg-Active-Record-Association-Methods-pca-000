a7f1afd51758e09e947f9a29da669d572b2f1bc9
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-06-17 15:20:10 -0400

    more practice

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,15 @@
+require 'pry'
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
   def get_genre_name
+    self.genre.name
   end
 
   def drake_made_this
+    artist = Artist.new(name: "Drake")
+    self.artist = artist
     # when this method is called it should assign the song's artist to Drake
   end
 end
